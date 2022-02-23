c891956dd3d432cf4473ddbf6751ef0f0b2f78e7
Athenkosi Gongotha <athenkosigongotha7@gmail.com>
2022-02-23 12:00:23 +0200

    created login and register page

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: () => import('../views/Products.vue')
+  },
 ]
 
 const router = createRouter({
